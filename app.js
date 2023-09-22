beb527ec2a6b7bba6c24a928097dca939e933fa8
Prachi Ramesh Godase <prachigodase2@gmail.com>
2023-09-22 11:30:32 +0530

    Request / Response

--- app.js
+++ app.js
@@ -1,46 +1,45 @@
 const http=require('http')
-const fs=require('fs')
 
 const server=http.createServer((req,res)=>{
+    // console.log(req.url,req.method,req.headers)
     const url=req.url
-    const method=req.method
     if(url==='/'){
-      
-        res.write("<html>")
-        res.write('<head><title>Enter Message</title><?head>')
-        res.write(`<body>${data}</body>`)
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
-        res.write("</html>")
-       return res.end()
-    }
-    if(url==='/message' && method==='POST'){
-        const body=[]
-        req.on('data',(chunk)=>{
-            body.push(chunk)
-        })
-        req.on('end',()=>{
-            const parsebody=Buffer.concat(body).toString()
-            const message=parsebody.split('=')[1]
-            fs.writeFile('message.txt',message, err=> {
-                // if (err) {
-                //     console.error(err);
-                //     res.statusCode = 500; // Internal Server Error
-                //     return res.end();
-                //   }
-                res.statusCode=302
-        res.setHeader('Location','/')
+        res.write('<html>')
+        res.write("<head><title>Enter Message</title></head>")
+        res.write("<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>")
+        res.write('</html>')
         return res.end()
-            })
-            
-        })
-    //  return
-        
     }
-    res.setHeader("Content-type",'text/html')
-    res.write("<html>")
-    res.write('<head><title>My First Paper</title></head>')
-    res.write('<body><h1>Hello from my NOde.js Server</h1></body>')
-    res.write("</html>")
+    if(url==='/home'){
+        res.write('<html>')
+        res.write('<html>')
+        res.write("<head><title>Welcome to home page</title></head>")
+        res.write('<body><h1>Welcome to home page</h1></body>')
+        res.write('</html>')
+        return res.end()
+    }
+    if(url==='/about'){
+        res.write('<html>')
+        res.write('<html>')
+        res.write("<head><title>Welcome to about page</title></head>")
+        res.write('<body><h1>Welcome to about page</h1></body>')
+        res.write('</html>')
+        return res.end()
+    }
+    if(url==='/node'){
+        res.write('<html>')
+        res.write('<html>')
+        res.write("<head><title>Welcome to node page</title></head>")
+        res.write('<body><h1>Welcome to node page</h1></body>')
+        res.write('</html>')
+        return res.end()
+    }
+    res.setHeader('Content-Type','text/html')
+    res.write('<html>')
+    res.write("<head><title>My first Page</title></head>")
+    res.write('<body><h1>Hello from my node.js server</h1></body>')
+    res.write('</html>')
     res.end()
 })
+
 server.listen(3000)
