0444ebc6da4c342e41fdb7d7aca39c1b4f846065
Prachi Ramesh Godase <prachigodase2@gmail.com>
2023-09-23 09:29:39 +0530

    Nodemon and Package JSON

--- routes.js
+++ routes.js
@@ -11,7 +11,7 @@ const requestHandler=((req,res)=>{
         res.write("<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>")
         res.write('</html>')
         return res.end()
-    
+     
     
     }
     if(url==='/message' && method==='POST'){
