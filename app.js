c8e816636cfc5a57d6ffca56e018a0841745a334
Prachi Ramesh Godase <prachigodase2@gmail.com>
2023-09-23 11:21:35 +0530

    started with Express

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-const http=require('http')
-const routes=require("./routes")
-console.log(routes.someText)
-const server=http.createServer(routes.handler)
-   
-   
-    
+// const http=require('http')
+const express=require('express')
+const app=express()
+app.use((req,res,next)=>{
+console.log("in the middleware")
+next() //allows the request to continue to the next middleware in line
+})
 
+app.use((req,res,next)=>{
+    console.log("in the another middleware")
+    res.send('<h1>Hello from express!</h1>')
+    // res.send("{ key1: value }")
+    })
+// const server=http.createServer(app)
+// server.listen(3000)
 
-server.listen(3001)
+app.listen(3000)
