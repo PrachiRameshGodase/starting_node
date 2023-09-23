0faaf792ae2b809919f4ae50bf28c49bc4843832
Prachi Ramesh Godase <prachigodase2@gmail.com>
2023-09-23 15:22:28 +0530

    Middlewares

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-// const http=require('http')
+
 const express=require('express')
+const bodyparser=require('body-parser')
 const app=express()
-app.use((req,res,next)=>{
-console.log("in the middleware")
-next() //allows the request to continue to the next middleware in line
+
+app.use(bodyparser.urlencoded({extended:false}))
+app.use('/app-product',(req,res,next)=>{
+ res.send("<form action='/product' method='POST'><input type='text'name='title'><button type='submit'>Add Product</button></form>")
+    
 })
 
-app.use((req,res,next)=>{
-    console.log("in the another middleware")
-    res.send('<h1>Hello from express!</h1>')
-    // res.send("{ key1: value }")
-    })
-// const server=http.createServer(app)
-// server.listen(3000)
+app.post('/product',(req,res,next)=>{
+    console.log(JSON.stringify(req.body)); 
+    res.redirect('/')
+})
+app.use('/',(req,res,next)=>{
+  res.send('<h1>Hello from express!</h1>')
+    
+})
+
 
 app.listen(3000)
