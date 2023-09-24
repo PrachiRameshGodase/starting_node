dc4b54caed59ff2893b9eacba530e1ffafb4715f
Prachi Ramesh Godase <prachigodase2@gmail.com>
2023-09-24 13:06:39 +0530

    Serving HTML , CSS and JS (VIEWS)

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const path=require('path')
+const path = require("path");
 const express = require("express");
 
 const bodyparser = require("body-parser");
@@ -6,14 +6,18 @@ const bodyparser = require("body-parser");
 const app = express();
 const shopRoutes = require("./routes/shop");
 const adminRoutes = require("./routes/admin");
+const contactRoutes=require("./routes/contactus")
 
 app.use(bodyparser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname,"public")))
-app.use('/admin',adminRoutes);
+
+//addind css path
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(contactRoutes)
 
-
-app.use((req,res,next)=>{
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'))
-})
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
 app.listen(3000);
